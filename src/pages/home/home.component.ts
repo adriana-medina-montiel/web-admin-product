04bcf040a24a8fea42aee6a65eccae154c12789d
adriana <adrimedinamontiel12@gmail.com>
2025-06-30 09:52:33 -0600

    changes

---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
