c08ac7af34ccee4dcfa4f8f0c3c7ed26b34c8b9f
adriana <adrimedinamontiel12@gmail.com>
2025-11-04 09:45:08 -0600

    changes

---
+++ src/pages/perfiles/perfiles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfiles',
+  imports: [],
+  templateUrl: './perfiles.component.html',
+  styleUrl: './perfiles.component.css'
+})
+export class PerfilesComponent {
+
+}
